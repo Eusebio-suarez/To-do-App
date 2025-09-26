07b452817eeca9861d05bd73552efd23c2187a24
eusebio suarez <eusebiosuaresmartines@gmail.com>
2025-09-25 21:14:18 -0500

    ver detalles y formulario para actualizar tareas

---
+++ src/app/modules/board/components/task-details/task-details.component.ts
@@ -0,0 +1,52 @@
+import { Component, EventEmitter, Input, Output, SimpleChange, SimpleChanges } from '@angular/core';
+import { TaskResponse } from '../../models/response/taskResponse';
+import { FormBuilder, Validators } from '@angular/forms';
+import { OnChanges } from '@angular/core';
+
+@Component({
+  selector: 'app-task-details',
+  templateUrl: './task-details.component.html',
+  styleUrls: ['./task-details.component.css']
+})
+export class TaskDetailsComponent implements OnChanges {
+
+  @Input()
+  task:TaskResponse|null = null
+
+  @Input()
+  isOpen:boolean = false
+
+  @Output()
+  close = new EventEmitter<void>
+
+  constructor(private fb:FormBuilder){
+
+  }
+
+  formUpdate = this.fb.group({
+    "title": ["",[Validators.required]],
+    "description" : ["",[Validators.required]],
+    "status":["",[Validators.required]]
+  }) 
+
+  //se ejecuta cada que detecta cambios en las propiedades de entrada
+  ngOnChanges(changes:SimpleChanges){
+
+    this.formUpdate.patchValue({
+      title:this.task?.title,
+      description:this.task?.description,
+      status:this.task?.status
+    })
+    
+  }
+
+  closeModal(){
+    this.close.emit()
+  }
+
+  handlerSubmit(){
+    console.log(this.task?.id)
+    console.log(this.formUpdate.value)
+  }
+
+}
